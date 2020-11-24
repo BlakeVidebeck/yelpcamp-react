910fdc12848e883b18099bebf932dc9a441fafbc
BlakeVidebeck <blakevidebeck@gmail.com>
2020-11-24 14:40:37 +1300

    add heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 
@@ -24,11 +23,22 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/campgrounds', require('./routes/api/campgrounds'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set Static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-	)
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+			.bold,
+	),
 );
